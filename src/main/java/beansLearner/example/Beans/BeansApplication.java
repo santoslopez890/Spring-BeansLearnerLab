a61710e82094e2399955f1729b4d4fb2922e0a0f
santos <santoslopez890@gmail.com>
2023-08-18 14:20:38 -0400

    did step 0

---
+++ src/main/java/beansLearner/example/Beans/BeansApplication.java
@@ -0,0 +1,15 @@
+package beansLearner.example.Beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beansLearner/example/Beans/BeansApplicationTests.java
@@ -0,0 +1,13 @@
+package beansLearner.example.Beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
